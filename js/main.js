b6e841a07469de5cab058f4de095918bac833124
talover <taloer9@gmail.com>
2014-09-15 09:42:42 +0300

    photos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
 	$('input, select').styler();  
+
+	$('.photo-list a').click(function(){
+		var photo = $(this).data('photo');
+
+		$('.photo-list li ,.photo-tabs').removeClass('active');
+		$(this).parent().addClass('active');
+		$(photo).addClass('active');
+	});
 });
