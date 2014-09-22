b27ab21333c02877da1efaffa008f4302bd8eaf8
talover <taloer9@gmail.com>
2014-09-22 14:56:42 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -8,4 +8,55 @@ $(document).ready(function(){
 		$(this).parent().addClass('active');
 		$(photo).addClass('active');
 	});
+
+	$('.contacts a').click(function(){
+		var contacts = $(this).data('contacts');
+
+		$('.contacts-tab, .contacts a').removeClass('active');
+
+		$(this).addClass('active');
+		$(contacts).addClass('active');
+	});
+
+	cashless_payment();
+	
+	$('.payment-block input[type="radio"]').change(function(){
+		cashless_payment()
+	});
+
+	$('.photo-gallery-list li').mouseenter(function(){
+		$(this).addClass('active').siblings().removeClass('active');
+	});
+
+	$('.price-block table a').click(function(){
+		$(this).siblings('.slide-block').slideToggle(function(){
+			$(this).parents('tr').toggleClass('active');
+		});
+	});
+
+	/*-- num --*/
+
+	$('.num-block  a.up').click(function(){
+		var num = $(this).siblings('input[type="text"]').val();
+
+		$(this).siblings('input[type="text"]').val(parseInt(num)+1);
+	});
+
+	$('.num-block  a.down').click(function(){
+		var num = $(this).siblings('input[type="text"]').val();
+
+		if(num >1) {
+			$(this).siblings('input[type="text"]').val(parseInt(num)-1);
+		}else {
+			$(this).siblings('input[type="text"]').val(1);
+		}
+	});
 });
+
+function cashless_payment() {
+	if($('#cashless-payment').is(":checked")) {
+		$('.cashless-payment').slideDown();
+	}else {
+		$('.cashless-payment').slideUp();
+	}
+}
